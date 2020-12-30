b89c43ec44e4a71c4bfaa20f5b6b40c03dd0108d
zebing <zebing.fu@desz9.com>
2020-12-30 17:52:32 +0800

    feat: 初始化

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-const arr: string[] = ['skm', 'lksdf'];
-console.log(arr)
+import * as BabelTypes from '@babel/types';
+
+export default function ({ types: BabelTypes }): any {
+
+  return {
+    name: 'babel-plugin-vue-css-modules',
+    visitor: {
+      Program: {
+        enter (path1, state) {
+          console.log(path1)
+        }
+      },
+
+      FunctionDeclaration (path)  {
+        console.log(path)
+      }
+    }
+  }
+}
